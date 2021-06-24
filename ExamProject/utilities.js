260bd551600f65a56e13d4fe5a29a3b1eda2f936
MaybeMike1 <57484151+MaybeMike1@users.noreply.github.com>
2021-06-24 20:40:16 +0100

    Final Version

--- ExamProject/utilities.js
+++ ExamProject/utilities.js
@@ -5,7 +5,7 @@ function animate() {
     ctx4.clearRect(0,0, canvas.width, canvas.height);
     ctx5.drawImage(background,0,0,canvas.width, canvas.height);
     
-    
+    handleScoreBoard();
     handleObstacles();
     character.draw();
     character.update();
@@ -13,7 +13,7 @@ function animate() {
     handleRipples();
     
     
-    handleScoreBoard();
+    
     
     ctx4.drawImage(grass,0,0,canvas.width, canvas.height);
     frame++;
@@ -28,14 +28,39 @@ window.addEventListener('keydown', (event) => {
     keys = [];
     keys[event.key] = true;
     if (keys["ArrowLeft"] || keys["ArrowUp"] || keys["ArrowRight"]  || keys["ArrowDown"]) {
-        character.move();
-        console.log('Sound should come out');
-        let synth = new Tone.Synth().toDestination();
-        synth.triggerAttackRelease("C4", "8n");
-       
+        character.move(); 
     };
     
 });
+
+window.addEventListener('keydown', (event) => {
+    const synth = new Tone.Synth().toDestination();
+
+    
+    try{
+        if (event.key == 'ArrowLeft') {
+            synth.triggerAttackRelease(nodesArr[3][0] , nodesArr[3][1]);
+        }
+
+        if (event.key == 'ArrowRight') {
+            synth.triggerAttackRelease(nodesArr[2][0], nodesArr[2][1]);
+        }
+        
+        if (event.key == 'ArrowUp') {
+            synth.triggerAttackRelease(nodesArr[0][0], nodesArr[0][1]);
+        }
+        
+        if (event.key == 'ArrowDown') {
+            synth.triggerAttackRelease(nodesArr[1][0], nodesArr[1][1]);
+        }
+    }catch(ReferenceError) {
+        console.log('Sound couldnt play due to missing values in sound config');
+    }
+    
+
+});
+
+
 
 window.addEventListener('keyup', (event) => {
     delete keys[event.key];
@@ -49,6 +74,36 @@ function scored() {
     character.y = canvas.height - character.height - 40;
 }
 
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+let nodesArr = []
+
+const submitBottun = document.getElementById("submitButton");
+submitBottun.addEventListener('click', () => {
+    nodesArr = nodesToArray();
+})
+
+
+function nodesToArray() {
+        let soundUp = document.getElementById("soundUp").value.split(" ");
+        let soundDown = document.getElementById("soundDown").value.split(" ");
+        let soundLeft = document.getElementById("soundLeft").value.split(" ");
+        let soundRight = document.getElementById("soundRight").value.split(" ");
+        
+        nodesArr.push(soundUp, soundDown, soundRight, soundLeft);
+    
+    console.log(nodesArr)
+
+    return nodesArr;
+}
+
+
+
 function handleScoreBoard() {
     ctx4.fillStyle = 'black';
     ctx4.font= '15px Verdana';
@@ -57,8 +112,9 @@ function handleScoreBoard() {
     ctx4.fillText(score + 1, 270, 65);
     ctx4.font = '15px Verdana';
     ctx4.strokeText('Collisions: ' + collisionCount, 10,175)
-    ctx4.strokeText('Game Speed: ' + gameSpeed.toFixed(1), 10,195)
+    ctx4.strokeText('Game Speed: ' + gameSpeed.toFixed(1), 10,195);
 }
+
 //Character is first and obstacles on road is second
 function collistion(first, second) {
     return  !(  first.x > second.x + second.width ||
@@ -67,7 +123,16 @@ function collistion(first, second) {
                 first.y + first.height < second.y);
 }
 
+function playDeathSound() {
+    const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+    const now = Tone.now();
+    synth.triggerAttackRelease('A1', '8n');
+    synth.triggerAttackRelease('C3', '8n');
+    synth.triggerAttackRelease('F2', '8n')
+}
+
 function resetGame() {
+    playDeathSound();
     character.x = canvas.width/2 - character.width/2;
     character.y = canvas.height - character.height - 40;
     score = 0;
